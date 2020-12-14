343444ea6be39a7514d8e2df57dd0443d4c5017c
Russell Cohen <rcoh@amazon.com>
2020-12-14 16:31:31 -0500

    dynein 0.1.2 (new formula)
    This commit adds the dynein formula, a CLI client for Dynamo DB. The dynein client installs the `dy` executable. For clarity, `dy` is also symlinked as `dynein`.

---
+++ Formula/dynein.rb
@@ -0,0 +1,22 @@
+class Dynein < Formula
+  desc "DynamoDB CLI written in Rust"
+  homepage "https://github.com/awslabs/dynein"
+  url "https://github.com/awslabs/dynein/archive/v0.1.2.tar.gz"
+  sha256 "701ed6d6a05fafa6bc54339bad6c84c8b85fc4c3d2d252f21415e047cbdc501c"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bin.install_symlink "dy" => "dynein"
+  end
+
+  test do
+    require "open3"
+    assert_match "admin", shell_output("#{bin}/dynein --help")
+    assert_match "admin", shell_output("#{bin}/dy --help")
+    _, stderr = Open3.capture3("#{bin}/dy", "ls")
+    assert_match "Couldn't find AWS credentials", stderr
+  end
+end
